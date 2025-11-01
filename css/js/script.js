e0e2393967065b50617b3b11f7b35a659a1e9b1d
OssieFromDK <mrcraftonyt@gmail.com>
2025-11-01 17:47:35 -0500

    Delete css directory

--- css/js/script.js
+++
@@ -1,6 +0,0 @@
-// Example: Load leaderboard via fetch (uncomment when you have an API)
-// fetch('api/leaderboard.json')
-//   .then(res => res.json())
-//   .then(data => renderLeaderboard(data));
-
-console.log("Gooners Leaderboard ready!");
