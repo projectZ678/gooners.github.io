7784dc1449d96043f232e246d9c502d6e64924c3
OssieFromDK <mrcraftonyt@gmail.com>
2025-11-01 17:33:11 -0500

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,6 @@
+// Example: Load leaderboard via fetch (uncomment when you have an API)
+// fetch('api/leaderboard.json')
+//   .then(res => res.json())
+//   .then(data => renderLeaderboard(data));
+
+console.log("Gooners Leaderboard ready!");
